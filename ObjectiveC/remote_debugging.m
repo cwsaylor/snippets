e845cd8792f26d1362b40fd9cb498e9e53255e69
Chris Saylor <cwsaylor@gmail.com>
2012-03-06 14:53:55 -0800

    Adding code for remote debugging of an iPhone app

---
+++ ObjectiveC/remote_debugging.m
@@ -0,0 +1,3 @@
+// Find your application:didFinishLaunchingWithOptions method on your AppDelegate.m file and add this line of code inside:
+// Remove me after debugging
+[NSClassFromString(@"WebView") _enableRemoteInspector];
