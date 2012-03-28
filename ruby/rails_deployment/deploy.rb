f118eeab7b01debea05276a23c1e635f1bbea6d4
Chris Saylor <cwsaylor@gmail.com>
2012-03-27 21:03:47 -0700

    Adding deploy stuff

--- ruby/rails_deployment/deploy.rb
+++ ruby/rails_deployment/deploy.rb
@@ -50,4 +50,13 @@ namespace :deploy do
     end
   end
   before "deploy", "deploy:check_revision"
+
+  desc "tail production log files" 
+  task :tail_logs, :roles => :app do
+    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:host]}: #{data}" 
+      break if stream == :err    
+    end
+  end  
 end
